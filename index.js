d8e4428de09a19aee62c69e5b52a0eab7f426090
YousefAboAisha <74072781+YousefAboAisha@users.noreply.github.com>
2021-06-24 09:38:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById("toggle");
+const navBar = document.querySelector(".nav-bar");
+const sideBar = document.querySelector(".side-bar");
+const video = document.querySelector(".main-page .videos");
+const vidCards = document.querySelectorAll(".videos .card");
+const vidIcons = document.querySelectorAll(".videos .card .icons i");
+
+// toggleBtn.addEventListener("click", function () {
+//   navBar.classList.toggle("hide");
+//   sideBar.classList.toggle("show");
+// });
